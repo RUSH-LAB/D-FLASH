44b05736854e4dac8ba2e0f86eb0687d7de81b59
Meisburger <ncm5@login2.nots.rice.edu>
2019-10-15 16:19:23 -0500

    add setup bash script

--- src/FLASH.cpp
+++ src/FLASH.cpp
@@ -20,20 +20,21 @@
 #include <iostream>
 
 void checkFileIO() {
+	int vectorsPerNode = 85000;
 	int provided;
 	MPI_Init_thread(0, 0, MPI_THREAD_FUNNELED, &provided);
 	int myRank, worldSize;
 	MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 
-	int* sparseMarkers = new int[11];
-	int* sparseIndices = new int[10 * 5000];
-	float* sparseVals = new float[10* 5000];
+	int* sparseMarkers = new int[vectorsPerNode + 1];
+	int* sparseIndices = new int[vectorsPerNode * 5000];
+	float* sparseVals = new float[vectorsPerNode * 5000];
 
 	for (int n = 0; n < worldSize; n++) {
 		if (myRank == n) {
 			printf("Reading File Node %d\n", n);
-			readSparse(BASEFILE, 10 * myRank, 10, sparseIndices, sparseVals, sparseMarkers, 10 * 5000);
+			readSparse(BASEFILE, vectorsPerNode * myRank, vectorsPerNode, sparseIndices, sparseVals, sparseMarkers, vectorsPerNode * 5000);
 		}
 		MPI_Barrier(MPI_COMM_WORLD);
 	}
---
+++ src/setup.sh
@@ -0,0 +1,4 @@
+module purge
+module load StdEnv
+module load GCC/6.4.0
+module load OpenMPI/2.1.3
